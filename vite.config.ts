84ee4880ef7b585f06bfd34606f186932b3da1c5
Lucien Bangelesa <ir.bangelesa@gmail.com>
2025-09-05 18:44:42 +0100

    public access

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -14,5 +13,19 @@ export default defineConfig({
       '@views': fileURLToPath(new URL('./src', import.meta.url)),
       '@stores': fileURLToPath(new URL('./src', import.meta.url)),
     }
+  },
+  // Ajout de la configuration pour le mode 'preview'
+  preview: {
+    host: '0.0.0.0', // Permet au serveur d'écouter sur toutes les interfaces réseau
+    port: 8080, // Assurez-vous que le port correspond à celui de votre configuration DigitalOcean
+    strictPort: true, // Force le port spécifié
+    allowedHosts: [
+      'lobster-app-z282q.ondigitalocean.app' // Ajoutez le nom de domaine de votre application DigitalOcean
+    ]
+  },
+  // Ajout de la configuration pour le mode 'dev' pour la cohérence
+  server: {
+    host: '0.0.0.0',
+    port: 8080
   }
 })
